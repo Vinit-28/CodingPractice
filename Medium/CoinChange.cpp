e05c8f17050693e20395e734afe16a4e96e6cafd
Vinit-28 <codervinit@gmail.com>
2021-04-30 16:16:33 +0530

    Solved Coin Change Problem --Practice

---
+++ Medium/CoinChange.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+
+    public :
+
+        int Get_Combinations_Count(vector<int> arr, int target_sum){
+
+            vector<vector<int>> dp (arr.size()+1);
+            int i,j;
+
+            for( i =0; i < dp.size(); i++){
+
+                dp[i] = (vector<int>(target_sum+1));
+
+                {
+
+                    for( j=0; j<dp[i].size(); j++)
+                        if ( i==0 && j==0 )
+                            dp[i][j] = 1;
+                        else
+                            dp[i][j] = 0;
+                }
+            }
+            
+
+            for ( i=1; i<dp.size(); i++){
+
+                for ( j=0; j<dp[i].size(); j++){
+
+                    if ( arr[i-1] > j ){
+
+                        dp[i][j] = dp[i-1][j];
+                    }
+                    else{
+
+                        dp[i][j] = (dp[i-1][j]) + (dp[i][j-arr[i-1]]);
+                    }
+                }
+            }
+            // cout<<endl;
+            // for ( i =0 ; i< dp.size(); i++){
+
+            //     for ( j=0; j< dp[i].size(); j++)
+            //         cout<<dp[i][j]<<"\t";
+
+            //     cout<<endl;
+            // }
+
+            // cout<<endl;
+
+            return dp[arr.size()][target_sum];
+        }
+};
+
+
+int main()
+{
+    
+    vector<int> arr{1,2,3,5,7,9};
+    int target_sum = 15;
+
+    Solution obj;
+    cout<<obj.Get_Combinations_Count(arr,target_sum);
+    cout<<endl;
+    return 0;
+}
