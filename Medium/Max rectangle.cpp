0ea1d410d36651b61a6a25dc5e7bddb2138d6a74
Vinit-28 <codervinit@gmail.com>
2021-07-11 09:49:28 +0530

    Solved Max Rectangle Problem

---
+++ Medium/Max rectangle.cpp
@@ -0,0 +1,77 @@
+
+
+// Question Link :: "https://practice.geeksforgeeks.org/problems/max-rectangle/1"
+
+
+// Solution //
+
+class Solution{
+  public:
+  
+    // Getting Smallest Values in Left aand Right Direction //
+    vector<int> nextSmallestElement(int arr[], int n, char direction){
+        // Your code here
+        int len = 0,i;
+        vector<int> stack, ans(n);
+        i = (direction == 'L')? (n-1) : (0);
+        
+        while( (direction == 'L' && i>=0) || (direction == 'R' && i<n) ){
+            
+            if( (direction == 'L' && i == n-1) || ( direction == 'R' && i == 0 ))
+                stack.push_back(i),len+=1,ans[i] = -1;
+            
+            else{
+                
+                while( len>0 && ( (direction=='L' && arr[stack[len-1]] > arr[i]) || (direction=='R' && arr[stack[len-1]] >= arr[i]) ) )
+                    stack.pop_back(),len-=1;
+             
+                if( len <= 0 ){
+                    stack.push_back(i),len+=1,ans[i] = -1;
+                    (direction=='L')? (i--):(i++);
+                    continue;
+                }
+                
+                ans[i] = stack[len-1],stack.push_back(i),len+=1;
+            }
+         
+            (direction=='L')? (i--):(i++);
+            
+        }
+        return ans;
+    }
+    
+    
+    //Function to find largest rectangular area possible in a given row/histogram.
+    int getMaxArea(int arr[], int n)
+    {
+        // Your code here
+        auto rightSmallest = nextSmallestElement(arr, n, 'L'); // Getting Smallest values on the Right Side by Moving Left Side //
+        auto leftSmallest = nextSmallestElement(arr, n, 'R'); // Getting Smallest values on the Leftt Side by Moving Right Side //
+        int ans  = 0, currVal=0;
+       
+        for(int i=0;i<n;i++){
+            
+            currVal = ( leftSmallest[i] == -1 )? ((i+1)*arr[i]) : (abs(i-leftSmallest[i])*arr[i]);
+            currVal += ( rightSmallest[i] == -1 )? (abs((n-i)-1)*arr[i]) : (abs((rightSmallest[i]-i)-1)*arr[i]);
+    
+            ans = max(ans, currVal);
+        }
+        return ans;
+    }
+  
+    int maxArea(int mat[MAX][MAX], int n, int m) {
+        // Your code here
+        
+        int maxAns = INT_MIN;
+        maxAns = max(maxAns, getMaxArea(mat[0], m));
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++)
+                if( mat[i][j] )
+                    mat[i][j] += mat[i-1][j]; 
+            maxAns = max(maxAns, getMaxArea(mat[i], m));
+        }
+        
+        return maxAns;
+    }
+};
